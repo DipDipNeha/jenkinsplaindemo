8390ac055e65e51a01c21b7e07daa346b1d6ea2d
Dipak <kumardipak962@gmail.com>
2025-05-04 16:12:50 +0530

    second commit

--- src/test/java/com/jenkins/demo/SpringBootJenkinsPlainApplicationTests.java
+++ src/test/java/com/jenkins/demo/SpringBootJenkinsPlainApplicationTests.java
@@ -1,13 +1,21 @@
 package com.jenkins.demo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class SpringBootJenkinsPlainApplicationTests {
 
 	@Test
 	void contextLoads() {
+		System.err.println("Hello Tested");
+		assertEquals(true, true);
+		
 	}
 
 }
